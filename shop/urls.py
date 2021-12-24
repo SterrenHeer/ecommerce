d96e7b299733d96b5cbe52365b0307e51a07ccbe
SterrenHeer <sterrenheer@gmail.com>
2021-12-24 15:34:10 +0300

    Add removing and deleting products from the cart features and fix adding feature

--- shop/urls.py
+++ shop/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     path('category/<slug:category_slug>/<slug:product_slug>', views.product, name='product_detail'),
     path('cart', views.cart_detail, name='cart_detail'),
     path('cart/add<int:product_id>', views.add_cart, name='add_cart'),
+    path('cart/remove<int:product_id>', views.cart_remove, name='cart_remove'),
+    path('cart/delete<int:product_id>', views.cart_delete, name='cart_delete'),
 ]
